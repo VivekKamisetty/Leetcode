58925062330c0717b46fe1f0dc73d4586728205b
Mr@UnKnOwN <44204514+VivekKamisetty@users.noreply.github.com>
2023-07-25 20:02:09 -0400

    Create Search_Insert_Postion.py

---
+++ Arrays/Search_Insert_Postion.py
@@ -0,0 +1,10 @@
+#https://leetcode.com/problems/search-insert-position/
+class Solution:
+    def searchInsert(self, nums: List[int], target: int) -> int:
+
+        for i in range(len(nums)):
+            if(nums[i] == target):
+                return i
+            elif (nums[i] > target):
+                return i
+        return len(nums)
