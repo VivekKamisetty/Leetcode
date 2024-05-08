0a9bb076c67ea062fc89d8a581ea13fe008499e5
Mr@UnKnOwN <44204514+VivekKamisetty@users.noreply.github.com>
2024-05-08 14:33:46 -0400

    Update Subsets.py

--- 75_Problems/Backtracking/Subsets.py
+++ 75_Problems/Backtracking/Subsets.py
@@ -1,1 +1,24 @@
+#https://leetcode.com/problems/subsets/
+class Solution(object):
+    def subsets(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+        res = []
+        n = len(nums)
 
+        def bt(curr, start):
+            if len(nums) == start:
+                res.append(list(curr))
+                return
+            
+            curr.append(nums[start])
+            bt(curr, start+1)
+
+            curr.pop()
+
+            bt(curr, start+1)
+
+        bt([], 0)
+        return res
